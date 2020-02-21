2237c18b7a82137d03dd2d749096ca447c0d4845
KallicoFrois <froiskallico@gmail.com>
2020-02-21 16:20:57 -0300

    update MVP

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
+#%%
 from database import Database
 from pprint import pprint
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
+from functools import reduce
 
-
+#%%
 class App(object):
     def __init__(self):
+        # Instancia as variávies e data frames para rodar a análise
         self.db = Database()
         self.get_op()
         # # self.get_ops_em_analise()
         # # self.get_chicotes_em_analise()
-        # self.get_mps_em_analise()
+        self.get_mps_em_analise()
         # # self.get_fic_tec()
-        self.get_ocs_pendentes()
-        self.get_ops_pendentes()
+        # self.get_ocs_pendentes()
+        # self.get_ops_pendentes()
 
         # self.what_the_print()
 
+        # self.timeline()
+
     def get_op(self):
+        # Define o numero da(s) OPS(s) a ser(em) analisada(s)
         self.lista_ops = str((114562))
 
-    def listify_dataset(self, ds):
-        return str(ds.to_list()).replace("[", "(").replace("]", ")")
-
     def get_ops_em_analise(self):
+        # Busca do Banco de Dados de Origem as OPs para analise
+        #
+        # ANALISE
+        # |-OPS
+
         self.ops_em_analise = pd.read_sql(
             """
                 SELECT
@@ -46,6 +54,13 @@ class App(object):
             self.db.connection)
 
     def get_chicotes_em_analise(self):
+        # Obtém a lista de chicotes vinculados às OPs que estão sendo analisadas
+        #
+        # ANALISE
+        # |-OPS
+        #   |-CHICOTES
+        #
+
         self.chicotes_em_analise = pd.read_sql(
             """
                 SELECT
@@ -70,6 +85,14 @@ class App(object):
         )
 
     def get_mps_em_analise(self):
+        # Obtém a lista de Matérias Primas vinculadas aos chicotes em análise
+        #
+        # ANALISE
+        # |-OPS
+        #   |-CHICOTES
+        #     |-MATERIAS PRIMAS
+        #
+
         self.mp_em_analise = pd.read_sql(
             """
                 SELECT DISTINCT
@@ -95,7 +118,20 @@ class App(object):
             self.db.connection
         )
 
+        # Normaliza os percentuais a considerar dos estoques LP e Corte
+        self.mp_em_analise = self.mp_em_analise.fillna(value={"PERC_ESTOQUE_LP": 50}).replace({'PERC_ESTOQUE_LP': 0}, 50)
+        self.mp_em_analise = self.mp_em_analise.fillna(value={"PERC_ESTOQUE_CORTE": 50}).replace({'PERC_ESTOQUE_CORTE': 0}, 50)
+
+        # Aplica os percentuais a considerar em cada estoque (LP/Corte)
+        self.mp_em_analise["ESTOQUE_LP_CONSIDERADO"] = self.mp_em_analise["ESTOQUE_LP"] * (self.mp_em_analise["PERC_ESTOQUE_LP"]/100)
+        self.mp_em_analise["ESTOQUE_CORTE_CONSIDERADO"] = self.mp_em_analise["ESTOQUE_CORTE"] * (self.mp_em_analise["PERC_ESTOQUE_CORTE"]/100)
+
+        # Calcula o saldo inicial das matérias primas
+        estoques = ("ESTOQUE", "ESTOQUE_LP_CONSIDERADO", "ESTOQUE_CORTE_CONSIDERADO")
+        self.mp_em_analise["SALDO_INICIAL"] = sum([self.mp_em_analise[campo] for campo in estoques])
+
     def get_fic_tec(self):
+        # Obtém as fichas técnicas dos chicotes vinculados às OPs em análise
         self.fic_tec = pd.read_sql(
             """
                 SELECT
@@ -120,6 +156,7 @@ class App(object):
         )
 
     def get_ocs_pendentes(self):
+        # Obtem as ordens de compras pendentes para as MPs vinculadas à analise
         self.ocs_pendentes = pd.read_sql(
             """
                 SELECT DISTINCT
@@ -147,6 +184,7 @@ class App(object):
         )
 
     def get_ops_pendentes(self):
+        # Obtem as ordens de produção pendentes para as MPs vinculadas à analise
         self.ops_pendentes = pd.read_sql(
             # TODO: HERE in the SQL Query take the last day of "Semana" where ise_geral.entrega and ipe.entrega is null
             """
@@ -183,36 +221,59 @@ class App(object):
         # print("CHICOTES")
         # pprint(self.chicotes_em_analise)
         # pprint('-' * 120)
-        print("MP")
-        pprint(self.mp_em_analise)
-        pprint('-' * 120)
-        # print("FIC_TEC")
-        # pprint(self.fic_tec)
+        # print("MP")
+        # pprint(self.mp_em_analise)
         # pprint('-' * 120)
-        print("OCS")
-        pprint(self.ocs_pendentes)
-        pprint('-' * 120)
-        print("OPS")
-        pprint(self.ops_pendentes)
-        pprint('-' * 120)
+        # # print("FIC_TEC")
+        # # pprint(self.fic_tec)
+        # # pprint('-' * 120)
+        # print("OCS")
+        # pprint(self.ocs_pendentes)
+        # pprint('-' * 120)
+        # print("OPS")
+        # pprint(self.ops_pendentes)
+        # pprint('-' * 120)
 
-    def timeline(self):
-        self.datas = pd.DataFrame({"ENTREGA": pd.date_range(pd.datetime.today().strftime('%Y-%m-%d'), periods= 10)})
+#%%
+a = App()
 
-        # self.timeline = pd.merge(self.datas, self.ocs_pendentes[self.ocs_penentes["CPD_MP"] == 1062]["CPD_MP", "ENTREGA", "QTD_PENDENTE_OC"], on="ENTREGA", how='outer', sort="ENTREGA").merge(self.ops_pendentes[["CPD_MP", "ENTREGA", "COMPROMETIDO"]], 'outer', ["ENTREGA", "CPD_MP"])
-        ocs = self.ocs_pendentes[self.ocs_pendentes["CPD_MP"] == 1062][["CPD_MP", "ENTREGA", "QTD_PENDENTE_OC"]]
-        ops = self.ops_pendentes[self.ops_pendentes["CPD_MP"] == 1062][["CPD_MP", "ENTREGA", "COMPROMETIDO"]]
+#%%
+def timeline(self, CPD_MP=1062):
+    self.horizonte_programacao = self.mp_em_analise.loc[0]
 
-        # TODO here merge the OCS_PENDENTES and OPS_PENDENTES dataframe and calculate BALANCE for each day
-        # TODO Make it a "balance calculation function"
-        # TODO run this "balance calculation function" for each "MP em analise"
-
-
-        # print(ocs)
-        print('-'*120)
-        print(ops.to_string())
+    self.datas = pd.DataFrame({"ENTREGA": pd.date_range(
+        pd.datetime.today().strftime('%Y-%m-%d'), periods=10)})
 
 
 
+    ocs = self.ocs_pendentes[self.ocs_pendentes["CPD_MP"] == CPD_MP][["CPD_MP", "ENTREGA", "QTD_PENDENTE_OC"]].groupby(["CPD_MP", "ENTREGA"]).sum()
+    ops = self.ops_pendentes[self.ops_pendentes["CPD_MP"] == CPD_MP][["CPD_MP", "ENTREGA", "COMPROMETIDO"]].groupby(["CPD_MP", "ENTREGA"]).sum()
 
-App().timeline()
+    dfs_to_merge = [self.datas, ocs, ops]
+
+    # # TODO here merge the OCS_PENDENTES and OPS_PENDENTES dataframe and calculate BALANCE for each day
+    self.tl = reduce(lambda left, right: pd.merge(left, right, on="ENTREGA", how="outer", sort="ENTREGA"), dfs_to_merge)
+
+    estoques = [self.mp_em_analise[self.mp_em_analise["CPD_MP"] == CPD_MP][campo]
+                          for campo in ("ESTOQUE", "ESTOQUE_LP_CONSIDERADO", "ESTOQUE_CORTE_CONSIDERADO")]
+
+    SALDO_INICIAL = reduce(lambda a, b: sum(a+b), estoques)
+
+    self.tl.at[0, "SALDO_INICIAL"] = SALDO_INICIAL
+
+    # print("timeline")
+
+    pprint(self.tl.to_string())
+
+
+    # TODO Make it a "balance calculation function"
+    # TODO run this "balance calculation function" for each "MP em analise"
+
+
+
+setattr(a, 'timeline', timeline)
+
+a.timeline(a)
+
+
+# %%
