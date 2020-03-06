b648c1359d1af8768f2627639144a421e7bb33b4
Kallico Fróis <froiskallico@gmail.com>
2020-03-05 22:29:50 -0300

    parsing analyze.report from json to dict

--- datateck/feedStockAnalist/views.py
+++ datateck/feedStockAnalist/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from feedStockAnalist.scripts.app import App
 from .models import Analysis
 
+import json
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -11,16 +12,35 @@ class IndexView(generic.ListView):
     context_object_name = 'analysis_list'
 
     def get_queryset(self):
-        analysis = Analysis.objects.all()
-        return analysis
+        analysis_from_db = Analysis.objects.all()
+        queryset = []
 
-        # TODO: Here call App(csv=True, lista_ops=(114562)).analist() on analysis creation
-        # may not to call it here,
-        # may i need to call it from view and,
-        # from there, store it to analysis field
+        for analyze in analysis_from_db:
+            analyze_converted = self.convert_analyze_report_from_json_to_dict(analyze)
+            queryset.append(analyze_converted)
+
+        return queryset
+
+    def convert_analyze_report_from_json_to_dict(self, analyze):
+        analyze_report_as_dict = json.loads(analyze.report)
+        analyze_parsed = analyze
+        analyze_parsed.report = analyze_report_as_dict
+        return analyze_parsed
+
+
+
+
 
 class DetailView(generic.TemplateView):
     template_name = 'feedStockAnalist/detail.html'
 
     def get_queryset(self):
         return []
+
+class NewAnalyzeView(generic.CreateView):
+    pass
+
+    # TODO: Here call App(csv=True, lista_ops=(114562)).analist() on analysis creation
+    # may not to call it here,
+    # may i need to call it from view and,
+    # from there, store it to analysis field
