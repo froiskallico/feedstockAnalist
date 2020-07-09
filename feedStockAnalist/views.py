d6ddf119608ccc9359f18add770b72d5276a5a5b
froisKallico <froiskallico@gmail.com>
2020-07-09 16:10:15 -0300

    add NewRadarAnalyzeView

--- feedStockAnalist/views.py
+++ feedStockAnalist/views.py
@@ -56,7 +56,7 @@ class NewAnalyzeView(generic.FormView):
         except Exception as e:
             print(e)
             send_mail(
-            subject="feedstockAnalist - Deu merda!",
+            subject="feedstockAnalist - Erro na análise!",
             message="""
                 Oi.
 
@@ -83,6 +83,89 @@ class NewAnalyzeView(generic.FormView):
         analyze = App()
 
         items_to_analyze = analyze.get_list_of_items_to_analyze(production_orders_to_analyze_list=production_orders_to_analyze_list)
+
+        self.synthesis["items_to_analyze"] = items_to_analyze
+        self.synthesis["feedstock_items_count"] = len(items_to_analyze)
+        self.synthesis["missing_feedstock_items_count"] = 0
+        self.synthesis["total_cost_of_actions"] = 0
+        self.new_analyze.synthesis = self.synthesis
+        self.new_analyze.save()
+
+        for item in items_to_analyze:
+            report = analyze.timeline(item)
+            if len(report) > 0:
+                if "custo_acao_comprar" in report.keys():
+                    self.synthesis["total_cost_of_actions"] += report["custo_acao_comprar"]
+
+                if "custo_acao_antecipar" in report.keys():
+                    self.synthesis["total_cost_of_actions"] += report["custo_acao_antecipar"]
+
+                self.synthesis["missing_feedstock_items_count"] += 1
+                self.new_analyze.missingitems_set.create(report = report)
+
+        self.new_analyze.save()
+
+        return self.new_analyze.id
+
+
+class NewRadarItemsAnalyzeView(generic.TemplateView):
+    template_name = 'feedStockAnalist/new_radar_analyze.html'
+    success_url = reverse_lazy('feedStockAnalist:index')
+
+    def get(self, request):
+        self.new_analyze = Analysis(created_at=timezone.now(), created_by=self.request.user)
+
+        try:
+            user = User.objects.get(pk=self.request.user.id)
+            analyze_id = self.analyze()
+            send_mail(
+                subject="feedstockAnalist - Análise finalizada com sucesso.",
+                message="""
+                    Oi, {}
+
+                    Vim aqui só pra te avisar que a tua análise
+                    para os itens do Radar de Itens Críticos foi concluída com
+                    sucesso e já está disponível.
+
+                    Use o link http://app.datateck.com.br/detail/{}
+                    para acessá-la diretamente.
+                    """.format(
+                        str(user.first_name),
+                        str(analyze_id)
+                    ),
+                from_email="tri.inovacao@gmail.com",
+                recipient_list=['kallico@datateck.com.br', user.email]
+            )
+        except Exception as e:
+            print(e)
+            send_mail(
+            subject="feedstockAnalist - Erro na análise!",
+            message="""
+                Oi.
+
+                Vim aqui só pra te avisar que a tua análise
+                para os itens do Radar de Itens Críticos não foi concluída com
+                sucesso.
+
+                Tente novamente acessando: http://app.datateck.com.br/new
+
+                Detalhes do erro: {}
+                """.format(
+                    str(e)
+                ),
+            from_email="tri.inovacao@gmail.com",
+            recipient_list=['kallico@datateck.com.br', 'froiskallico@gmail.com']
+            )
+
+        return super(NewRadarItemsAnalyzeView, self).get(request)
+
+    def analyze(self):
+        self.synthesis = dict()
+        self.synthesis["production_orders_to_analyze_list"] = "Radar"
+
+        analyze = App()
+
+        items_to_analyze = analyze.get_list_of_items_to_analyze(analyze_radar_items=True)
 
         self.synthesis["items_to_analyze"] = items_to_analyze
         self.synthesis["feedstock_items_count"] = len(items_to_analyze)
